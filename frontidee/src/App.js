e23ea70baa1b87db7b9e5783e2dc32ed053b97d3
unknown <aichafatybittyba@gmail.com>
2022-03-23 17:25:11 +0000

    formulaire ajout des Idees

--- frontidee/src/App.js
+++ frontidee/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Formulaire from "./components/Formulaire";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+                    <Formulaire/>
       </header>
     </div>
   );
---
+++ frontidee/src/components/Formulaire.js
@@ -0,0 +1,36 @@
+import img1 from '../../public/img1.png';
+
+function Formulaire() {
+        return(
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6 col-sm-12 col-lg-4 ">
+                        <form>
+                            <div className="mb-3">
+                                <label htmlFor="titre" className="form-label">Titre</label>
+                                <input type="text" className="form-control" id="titre"/>
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="etat" className="form-label">Etat</label>
+                                <input type="text" className="form-control" id="etat"/>
+
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="description" className="form-label">Description</label>
+                                <textarea className="form-control" id="description" rows="3"></textarea>
+                            </div>
+
+
+                            <button type="submit" className="btn btn-primary" value="ajouter">Ajouter</button>
+                        </form>
+                    </div>
+                    <div className="col-md-6  col-sm-12 col-lg-4 ">
+                        <img src={img1} alt=""/>
+                    </div>
+                </div>
+            </div>
+        );
+
+
+        }
+export default Formulaire;
