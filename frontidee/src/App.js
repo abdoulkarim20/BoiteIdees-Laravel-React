3a39200638919617c9383401bf77cf83892ca1d6
jabbarDevOps <jabbar.devops@gmail.com>
2022-03-25 16:06:55 +0000

    suppression de mon travail pour remplacer celui des autres

--- frontidee/src/App.js
+++ frontidee/src/App.js
@@ -1,11 +1,11 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
-import Test from './components/Test';
+import Header from './components/Header';
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path='/' component={Test} />
+        <Route path='/' component={Header} />
       </Switch>
     </BrowserRouter>
   );
